a18f0da8225cca368ed0dffc9e280bae3046cbb8
Lindsey <lindseykaycarlson@outlook.com>
2022-10-25 19:27:25 -0700

    finished button changes is_done in backend, no front-end yet

--- ghi/app/src/service/ServiceAppointmentList.js
+++ ghi/app/src/service/ServiceAppointmentList.js
@@ -25,7 +25,8 @@ function ServiceAppointmentsList(props) {
                             <td>{appointment.time} </td>
                             <td>{appointment.technician.name} </td>
                             <td>{appointment.reason} </td>
-                            <td><button type="button" className="btn btn-danger" onClick={() => deleteItem(`${appointment.id}`)}>Cancel</button></td>
+                            <td><button type="button" className="btn btn-danger" onClick={() => deleteItem(`${appointment.href}`)}>Cancel</button></td>
+                            <td><button type="button" className="btn btn-success" onClick={() => finishItem(`${appointment.href}`)}>Finished</button></td>
                         </tr>
 
                     )
@@ -47,4 +48,18 @@ async function deleteItem(appointmentid) {
     };
     await fetch(appointmentUrl, fetchOptions);
     window.location.reload(true);
+  }
+
+  async function finishItem(appointmentid){
+    const appointmentUrl = `http://localhost:8080${appointmentid}`;
+    const fetchOptions = {
+        method: "put",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    await fetch(appointmentUrl, fetchOptions);
+    window.location.reload(true);
+
   }
