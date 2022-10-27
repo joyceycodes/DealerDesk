f4f7f58c7fa35ba16d4246973b2dc2dc3cff369c
Lindsey <lindseykaycarlson@outlook.com>
2022-10-27 12:54:17 -0700

    Automobile List view updated with extra info

--- ghi/app/src/inventory/AutomobilesList.js
+++ ghi/app/src/inventory/AutomobilesList.js
@@ -8,7 +8,9 @@ function AutomobilesList(props) {
                     <th>Year</th>
                     <th>Make</th>
                     <th>Model</th>
+                    <th>Color</th>
                     <th>Picture</th>
+                    <th>VIN</th>
                 </tr>
             </thead>
             <tbody>
@@ -18,7 +20,9 @@ function AutomobilesList(props) {
                             <td>{automobile.year}</td>
                             <td>{automobile.model.manufacturer.name} </td>
                             <td>{automobile.model.name}</td>
+                            <td>{automobile.color}</td>
                             <td><img style={{ width: 500 }} src={automobile.model.picture_url} alt="" /></td>
+                            <td>{automobile.vin}</td>
                         </tr>
                     )
                 })}
